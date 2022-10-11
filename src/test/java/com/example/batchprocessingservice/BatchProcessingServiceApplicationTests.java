99c8841534d998ec7a4139603dd32211789c1ff8
Konrad Kielbasa <konrad.kielbasa@ihsmarkit.com>
2022-10-11 12:07:02 +0200

    initial version

---
+++ src/test/java/com/example/batchprocessingservice/BatchProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
